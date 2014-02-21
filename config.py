8cfa5c19e07cc6e63fef80299c3fd28d4ae929db
Mike Kroutikov <pgmmpk@gmail.com>
2014-02-20 19:59:52 -0500

    fixed pin assignments (again!)

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ LEFT_MOTOR_PINS = 'P8_14', 'P8_16', 'P9_16'
 
 # IR sensors (clock-wise, starting with the rear left sensor):
 # rear-left, front-left, front, front-right, rear-right
-IR_PINS = ('P9_40', 'P9_38', 'P9_36', 'P9_35', 'P9_33')
+IR_PINS = ('P9_38', 'P9_40', 'P9_36', 'P9_35', 'P9_33')
 
 # Wheel encoder sensors: (left, right)
 ENC_PINS = ('P9_39', 'P9_37')
